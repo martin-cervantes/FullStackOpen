dd2c7f90dd58fd3d76bf2140fd2a5d074f40873c
Martin Cervantes <cervantes.martine@gmail.com>
2021-08-01 16:17:24 -0600

    Add exercise 1.10 step 5

---
+++ part1/unicafe/src/containers/Statistics.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import StatisticLine from '../components/StatisticLine';
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+
+  const average = (good - bad) / total;
+
+  const positive = good * 100 / total;
+
+  if (total === 0)
+    return <div><h1>Statistics</h1><p>No feedback given</p></div>
+
+  return (
+    <div>
+      <h1>Statistics</h1>
+
+      <StatisticLine text={"Good"} value={good} />
+      <StatisticLine text={"Neutral"} value={neutral} />
+      <StatisticLine text={"Bad"} value={bad} />
+      <StatisticLine text={"All"} value={total} />
+      <StatisticLine text={"Average"} value={average} />
+      <StatisticLine text={"Positive"} value={positive} />
+    </div>
+  )
+}
+
+export default Statistics;
