c012c563954fcb646a6b05f5cf5474cb62c3a115
Martin Cervantes <cervantes.martine@gmail.com>
2021-08-09 10:10:43 -0600

    Add exercise 2.1 Course information step 6

---
+++ part2/course_information/src/App.js
@@ -0,0 +1,71 @@
+import React from 'react'
+
+const Header = ({ course }) => {
+  return (
+    <h1>{course}</h1>
+  )
+}
+
+const Part = ({ data }) => {
+  const { name, exercises } = data;
+
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      { parts.map(part => <Part data={part} />) }
+    </div>
+  )
+}
+
+const Total = ({ parts }) => {
+  return (
+    <p>Number of exercises { parts.reduce((t, p) => t + p.exercises, 0) }</p>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+
+      <Content parts={course.parts} />
+
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
