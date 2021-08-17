8a136c3cd55954e9543b0f89047273346d4b9759
Martin Cervantes <cervantes.martine@gmail.com>
2021-08-17 12:50:24 -0600

    Add exercise 2.16 phonebook step 8

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import personsService from '../services/persons'
 
 import Filter from './Filter'
 import PersonForm from './PersonForm'
@@ -12,10 +12,10 @@ const App = () => {
   const [ search, setSearch ] = useState('')
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
+    personsService
+      .getAll()
+      .then(returnedPersons => {
+        setPersons(returnedPersons)
       })
   }, [])
 
@@ -38,11 +38,12 @@ const App = () => {
         number: newNumber,
       }
 
-      axios
-        .post('http://localhost:3001/persons', newPerson)
-        .then(response => {
+      personsService
+        .create(newPerson)
+        .then(returnedPersons => {
           setPersons([...persons, newPerson])
         })
+
       setNewName('')
       setNewNumber('')
     }
