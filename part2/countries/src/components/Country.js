447d07954e36e86c70434f6b86293285cf87990e
Martin Cervantes <cervantes.martine@gmail.com>
2021-08-16 18:47:11 -0600

    Add exercise 2.14 countries step 3

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,14 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const Country = ({ country }) => (
-  <div>
-    <h1>{country.name}</h1>
-    <img src={country.flag} width="200rem" alt="flag" />
-    <p>Capital: {country.capital}</p>
-    <p>Language: {country.languages[0].name}</p>
-    <p>Population: {country.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
-    <p>Currency: {country.currencies[0].name}</p>
-  </div>
-)
+const Country = ({ country, forescast }) => {
+  const [ forecast, setForecast ] = useState({})
+  const [ show, setShow ] = useState(false)
+
+  useEffect(() => {
+    if (!show) {
+      const api_key = process.env.REACT_APP_API_KEY
+
+      axios
+        .get(`http://api.openweathermap.org/data/2.5/weather?q=${country.capital}&units=metric&APPID=${api_key}`)
+        .then(response => {
+          setForecast(response.data)
+          setShow(true)
+        })
+    }
+  }, [forecast])
+
+  if (show) {
+    return (
+      <div>
+        <h1>{country.name}</h1>
+        <img src={country.flag} width="200rem" alt="flag" />
+        <p>Capital: {country.capital}</p>
+        <p>Language: {country.languages[0].name}</p>
+        <p>Population: {country.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+        <p>Currency: {country.currencies[0].name}</p>
+
+        <img src={`http://openweathermap.org/img/w/${forecast.weather[0].icon}.png`} alt="forecast" />
+        <p>Forecast: {forecast.weather[0].main}</p>
+        <p>Temperature: {forecast.main.temp}&deg;</p>
+        <p>Wind: {forecast.wind.speed} m/s</p>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h1>{country.name}</h1>
+      <img src={country.flag} width="200rem" alt="flag" />
+      <p>Capital: {country.capital}</p>
+      <p>Language: {country.languages[0].name}</p>
+      <p>Population: {country.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+      <p>Currency: {country.currencies[0].name}</p>
+    </div>
+  )
+}
 
 export default Country
